dc6361541f3654c0fc0755f42deca7ea83d13b46
Tiago Henrique Alves de Queiroz <tiago.external@kreios.lu>
2020-03-06 09:09:54 -0300

    add notifications styles

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -2,13 +2,34 @@ import React from 'react';
 
 import { MDNotifications } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, NotificationsList, Notification } from './styles';
 
 export default function Notifications() {
     return (
         <Container>
-            <Bags />
-            <MDNotifications color="#7159c1" size={20} />
+            <Bags hasUnread>
+                <MDNotifications color="#7159c1" size={20} />
+            </Bags>
+
+            <NotificationsList>
+                <Notification unread>
+                    <p>Voce possui um novo agendamento</p>
+                    <time>Há 2 dias</time>
+                    <button type="button">Marcar como lido</button>
+                </Notification>
+
+                <Notification>
+                    <p>Voce possui um novo agendamento</p>
+                    <time>Há 2 dias</time>
+                    <button type="button">Marcar como lido</button>
+                </Notification>
+
+                <Notification>
+                    <p>Voce possui um novo agendamento</p>
+                    <time>Há 2 dias</time>
+                    <button type="button">Marcar como lido</button>
+                </Notification>
+            </NotificationsList>
         </Container>
     );
 }
