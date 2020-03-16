d93b22ba2add01a7ef7baa19cee97eeb88ba6ff8
Tiago Henrique Queiroz <tiago.external@kreios.lu>
2020-03-16 15:59:38 -0300

    add header

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -2,13 +2,36 @@ import React from 'react';
 
 import { MDNotifications } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Badge, NotificationList, Scroll, Notification } from './styles';
 
 export default function Notifications() {
     return (
         <Container>
-            <Bags />
-            <MDNotifications color="#7159c1" size={20} />
+            <Badge hasOnRead>
+                <MDNotifications color="#7159c1" size={20} />
+            </Badge>
+
+            <NotificationList>
+                <Scroll>
+                    <Notification unRead>
+                        <p>Voce possui um agendamento para amanhã</p>
+                        <time>Há 2 dias</time>
+                        <button type="button">Marcar como lida</button>
+                    </Notification>
+
+                    <Notification>
+                        <p>Voce possui um agendamento para amanhã</p>
+                        <time>Há 2 dias</time>
+                        <button type="button">Marcar como lida</button>
+                    </Notification>
+
+                    <Notification>
+                        <p>Voce possui um agendamento para amanhã</p>
+                        <time>Há 2 dias</time>
+                        <button type="button">Marcar como lida</button>
+                    </Notification>
+                </Scroll>
+            </NotificationList>
         </Container>
     );
 }
